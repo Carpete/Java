0ee3599dd3efbcec89cf758c9b3022d585b87792
Carpete <75329581+Carpete@users.noreply.github.com>
2024-02-26 13:50:28 +0000

    Add files via upload

---
+++ AED/Aula2/src/QuickFindUF.java
@@ -0,0 +1,15 @@
+public class QuickFindUF {
+
+    public QuickFindUF(int n) {
+
+    }
+
+    public boolean connected(int p, int q) {
+
+    }
+
+    public void union(int p, int q) {
+
+    }
+
+}
